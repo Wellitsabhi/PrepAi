5a721ecaff3a8819d3c5a06aa5d35e5a10acafaf
wellitsabhi <abhishekongit@gmail.com>
2025-06-13 01:01:58 +0530

    auth pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ import React, {ReactNode} from 'react'
 // since each is an individual page, the type is ReactNode.
 const AuthLayout = ({children}: {children: ReactNode}) => {
   return (
-    <div>{children}</div>
+    <div className='auth-layout'>{children}</div>
   )
 }
 
